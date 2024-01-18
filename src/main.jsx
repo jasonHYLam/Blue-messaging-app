f8f6e8d25118c70708769af02cc87e22fb466477
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-18 14:08:54 +0000

    Add React Router To Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 // import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    // element: 
+    //PageLayout
+    children: [
+      {
+        path: 'login',
+        // login-signup
+      },
+      {
+        path: '/home',
+        // login-signup
+      },
+      {
+        path: '/home/chats/chatId',
+        // login-signup
+      },
+      {
+        path: '/home/userProfile/:userId',
+        // login-signup
+      },
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
