f003db647b97b40bbcbce7272566af1652d09204
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-27 14:11:46 +0000

    Change LoginSignupPage Name To LoginPage
    This is to support removing the LoginSignupPage, which is better as two
    separate pages in terms of routing.

---
+++ Components/LoginPage/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+
+export function LoginPage() {
+        // LoginFormComponent
+        // SignupFormComponent
+
+        // State for both
+
+        // State for submitted
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form action="">
+            </form>
+            <Link>New here? Signup</Link>
+        </>
+
+
+    )
+}
---
+++ Components/SignupPage/SignupPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export function SignupPage() {
+    return (
+        <>
+            <h1>Signup</h1>
+            <form action="">
+            </form>
+            <Link>Got an account? Login</Link>
+        </>
+    )
+}
