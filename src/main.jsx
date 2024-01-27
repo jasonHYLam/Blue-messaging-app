d1680b07416e741f43858a31bd427a83129fc8a9
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-27 14:13:42 +0000

    Add Login And Signup Elements To React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+
+import { LoginPage } from '../Components/LoginPage/LoginPage'
+import { SignupPage } from '../Components/SignupPage/SignupPage'
 // import './index.css'
 
 const router = createBrowserRouter([
@@ -12,10 +15,12 @@ const router = createBrowserRouter([
       {
         path: 'login',
         // login-signup
+        element: < LoginPage />
       },
       {
         path: 'signup',
         // login-signup
+        element: < SignupPage />
       },
       {
         path: '/home',
