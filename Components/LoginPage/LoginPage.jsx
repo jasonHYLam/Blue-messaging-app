eb9566db12bbdcdafe37c12b4b080dbf7cb86d88
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-28 19:31:29 +0000

    Start OnSubmit Function And Input For LoginForm

--- Components/LoginPage/LoginPage.jsx
+++ Components/LoginPage/LoginPage.jsx
@@ -10,18 +10,22 @@ export function LoginPage() {
         } = useForm();
 
         function onSubmit(data) {
+            console.log('checking data')
+            console.log(data)
 
         }
 
     return (
         <>
             <h1>Login</h1>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="username">Username</label>
                 <input type="text" id="username" {...register("username", {required: true})} />
                 
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" {...register("password", {required: true})} />
+
+                <input type="submit" />
                 
             </form>
             <Link>New here? Signup</Link>
