bf79c193e33a6f9ce7ea553cd8c33c964fb2e8bd
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-31 14:04:42 +0000

    Create Basic Fetch Request For Searching Users

--- Components/AddFriendWrapper/AddFriendWrapper.jsx
+++ Components/AddFriendWrapper/AddFriendWrapper.jsx
@@ -1,4 +1,28 @@
+import { useForm } from "react-hook-form"
+
 export function AddFriendWrapper() {
+
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
+
+    async function onSubmit(data) {
+        console.log('checking data for searching friend')
+        console.log(data)
+
+        const response = await fetch(``, {
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type" : "application/json",
+                // "Accept" : "application/json",
+                "Access-Control-Allow-Credentials": true,
+            },
+        })
+
+    }
     return (
         <>
         <p>Hi it's me, add friend wrapper</p>
@@ -6,7 +30,7 @@ export function AddFriendWrapper() {
         <p>Search for users to add:</p>
 
         <form action="">
-            <input type="text" />
+            <input type="text" {...register("username")}/>
             <input type="submit" />
         </form>
         
