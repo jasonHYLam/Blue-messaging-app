729232624e17afd1a6a69ef1c511cee8d296f253
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-31 11:12:16 +0000

    Add Form To AddFriendWrapper

--- Components/AddFriendWrapper/AddFriendWrapper.jsx
+++ Components/AddFriendWrapper/AddFriendWrapper.jsx
@@ -2,6 +2,13 @@ export function AddFriendWrapper() {
     return (
         <>
         <p>Hi it's me, add friend wrapper</p>
+
+        <p>Search for users to add:</p>
+
+        <form action="">
+            <input type="text" />
+            <input type="submit" />
+        </form>
         
         </>
     )
