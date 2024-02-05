a24393fb44fc6fc46b24a46d3deeb331a1d6f606
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-02-05 13:59:20 +0000

    Use React Hook Form For TypeBar Form

--- Components/ChatWrapper/TypeBar/TypeBar.jsx
+++ Components/ChatWrapper/TypeBar/TypeBar.jsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export function TypeBar() {
 
     const [ message, setMessage ] = useState('');
 
-    async function postMessage() {
+    const {
+        register,
+        handleSubmit
+    } = useForm();
 
-        const dataToPost = {message};
-        console.log('checking dataToPost')
-        console.log(dataToPost)
-        // const response = await fetch(`${ import.meta.env.VITE_BACKEND_URL }/home/chat/:chatid/create_message`, {
-        //     method: "POST",
-        //     mode: "cors",
-        //     credentials: "include",
-        //     headers: {
-        //         "Content-Type" : "application/json",
-        //         "Access-Control-Allow-Credentials": true,
-        //     },
-        //     body: JSON.stringify(dataToPost),
-        // })
+    async function postMessage(data) {
+
+        console.log('checking out data')
+        console.log(data)
+        // const dataToPost = {message};
+        // console.log('checking dataToPost')
+        // console.log(dataToPost)
+        const response = await fetch(`${ import.meta.env.VITE_BACKEND_URL }/home/chat/:chatid/create_message`, {
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type" : "application/json",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify(data),
+        })
     }
 
     return (
         <>
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            postMessage()
-            }}>
-            <input type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+        <form onSubmit={handleSubmit(postMessage)}>
+            {/* <input type="text" value={message} onChange={(e) => setMessage(e.target.value)}/> */}
+            <input type="text" {...register("message", {required: true})} />
             <input type="submit"/>
         </form>
         </>
