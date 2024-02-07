d4fcff5f0c9c1e1a715d31ac27be8cd5ef2aa627
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-02-07 13:31:27 +0000

    Create SubmitChange For UserProfileWrapper
    This will submit change to the relevant fetch URL in order to change a
    particular aspect of the user profile.

--- Components/UserProfileWrapper/UserProfileWrapper.jsx
+++ Components/UserProfileWrapper/UserProfileWrapper.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export function UserProfileWrapper() {
 
@@ -22,10 +23,14 @@ export function UserProfileWrapper() {
 
     // in order to do this, need to make a fetch request with the userID, which is perhaps derived from the params.
 
+    const {register, handleSubmit} = useForm();
     const [ isLoaded, setIsLoaded ] = useState(false);
     const [ isCurrentUser, setIsCurrentUser ] = useState(false);
     const [ userData, setUserData ] = useState({});
     const { userId } = useParams();
+
+    // currentStatus takes the following values: "editDescription", "editPassword", "editUsername"
+    const [ currentStatus, setCurrentStatus ] = useState('');
     console.log('checking isCurrent user')
     console.log(isCurrentUser)
 
@@ -54,7 +59,43 @@ export function UserProfileWrapper() {
 
     }, [])
 
-    const changeDescriptionButton = (<button>Change description</button>)
+    async function submitChange(data) {
+        const endRoute = (currentStatus === "editDescription") ? "change_description" : "";
+        const fetchURL = `/home/personal_profile/${endRoute}`;
+        console.log('checking data')
+        console.log(data)
+
+        // what do i submit?
+        // I may have to change form into react hook form
+        // const response = await fetch(`${ import.meta.env.VITE_BACKEND_URL }${fetchURL}`, {
+        //     method: "POST",
+        //     mode: "cors",
+        //     credentials: "include",
+        //     headers: {
+        //         "Content-Type" : "application/json",
+        //         // "Accept" : "application/json",
+        //         "Access-Control-Allow-Credentials": true,
+        //     },
+        // })
+
+        // I may need to refresh this page when change is submitted
+        // Man I absolutely detest these pages where you submit a change and need to refresh the page.
+    }
+
+    const cancelChangeButton = <button onClick={() => setCurrentStatus('')}>Cancel</button>
+    const changeDescriptionButton = (
+        currentStatus === 'editDescription' ? null 
+        : <button onClick={() => setCurrentStatus('editDescription')}>Change description</button>
+    )
+    const changeDescriptionForm = (
+        <>
+        <form onSubmit={handleSubmit(submitChange)}>
+            <input type="text" {...register('changeToSubmit')} />
+            <input type="submit" />
+        </form>
+        {cancelChangeButton}
+        </>
+    )
     
 
 
@@ -65,7 +106,10 @@ export function UserProfileWrapper() {
         <p>It's me the user profile page</p>
         <h1>User Profile: {userData.username}</h1>
         <p>Description:</p>
-        <p>{userData.description}</p>
+        {currentStatus === "editDescription" 
+        ? changeDescriptionForm 
+        : <p>{userData.description}</p>
+        }
         {isCurrentUser ? changeDescriptionButton : null}
 
         
