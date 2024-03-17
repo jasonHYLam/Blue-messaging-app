4c686fb77536df6b46544c0091b1e5d8e1509fd2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-17 17:40:32 +0000

    Change Color Of Link Text On Hover

--- src/Components/ToggleSidebar/ToggleSidebar.jsx
+++ src/Components/ToggleSidebar/ToggleSidebar.jsx
@@ -6,7 +6,6 @@ const TOGGLE_SIDEBAR_ICON_PATH = "../../../../menu.svg";
 export function ToggleSidebar({ setIsSidebarMinimised, isSidebarMinimised }) {
   return (
     <>
-
     <ClickableIcon 
      onClick={() => setIsSidebarMinimised(!isSidebarMinimised)}
      imgPath={TOGGLE_SIDEBAR_ICON_PATH}
