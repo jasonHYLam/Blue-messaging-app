bfc6aaff08fafb2c171cbdad2fcd770fc475a239
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-20 16:19:20 +0000

    Adjust Header Spacing And Button Color

--- src/Components/ChatWrapper/ChatMessage/ChatMessage.jsx
+++ src/Components/ChatWrapper/ChatMessage/ChatMessage.jsx
@@ -5,17 +5,12 @@ import { Link } from "react-router-dom"
 export function ChatMessage( { message, setMessageToReplyTo }) {
 
   const DEFAULT_PROFILE_PIC_PATH = "../../../../defaultProfilePic";
-    // should contain:
 
     // message author username and profile image.
     // message text
     // sent when (few minutes ago, a day ago)
-    // date when hovering on it. Does that require state?
-    // reactions
     // if the message is a reply, then one line of the message that it is replying to, above.
-    // if hovering, then buttons to allow reaction or reply.
-
-    // if making a reply or a reaction, then that will need to update the message in the database.
+    // if hovering, then buttons to allow reply.
 
     const profilePicPath = message.author.profilePicURL ? message.author.profilePicURL : DEFAULT_PROFILE_PIC_PATH;
 
--- src/Components/ProfilePic/ProfilePic.jsx
+++ src/Components/ProfilePic/ProfilePic.jsx
@@ -1,9 +1,14 @@
 import styles from "./ProfilePic.module.css"
 
+const DEFAULT_PROFILE_PIC_PATH = "../../../defaultProfilePic.svg";
+
 export function ProfilePic({imgPath}) {
   return (
     <>
-      <img className={styles.profilePic} src={imgPath} alt="" />
+    {!imgPath ? <img className={styles.defaultProfilePic} src={DEFAULT_PROFILE_PIC_PATH} alt="" /> : 
+    <img className={styles.profilePic} src={imgPath} alt="" />
+    }
+
     </>
   )
 }
