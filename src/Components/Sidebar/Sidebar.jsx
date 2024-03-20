b4436d137486af902d31848a211dd60e28852e1e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-20 17:07:34 +0000

    Remove Comments And Unused Imports From Sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
 import styles from './Sidebar.module.css';
-import { useNavigate, Outlet, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { ChatPreview } from './ChatPreview/ChatPreview';
-import { ToggleSidebar } from '../ToggleSidebar/ToggleSidebar';
 import { ClickableIcon } from '../ClickableIcon/ClickableIcon';
 
-// import icon from "../../../public/user-add"
 const ADD_FRIEND_ICON_PATH = "../../../user-add.svg";
 const CREATE_CHAT_ICON_PATH = "../../../comment.svg";
 
@@ -16,30 +14,18 @@ export function Sidebar({chatsList, isSidebarMinimised }) {
 
     const sidebarClass = `${styles.sidebar} ${styles.open}` ;
 
-    // const [ allChats, setAllChats ] = useState([]);
-
-    // console.log('checking allChats')
-    // console.log(allChats)
-
     useEffect(() => {
         setIsLoaded(true);
     },[])
 
 
-    // to store list of chatLinks
-    // should make fetch request to get all chats, perhaps updated by time (most recently accessed at the top)
     // so perhaps each chat should have a lastUpdated field. When making a comment, update this field.
-    // requires useState and useEffect frankly
-    // perhaps the chat should also have a field for the number of members.
     return (
         !isLoaded ? <p>Loading</p> :
 
         <>
-
             <section className={sidebarClass}>
-
               {isSidebarMinimised ? null :
-              
               <>
               
                 <section className={ styles.buttonsContainer }>
